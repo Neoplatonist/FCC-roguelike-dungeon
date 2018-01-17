0c247741ee7d21c63a9bff6790be3a2be6fcd716
Neoplatonist <mr.neoplatonist@gmail.com>
2018-01-17 12:12:33 +0900

    added start/stop btns, cleaned main, and restarted map creation algorithm

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,9 +1,4 @@
 import React, {Component} from 'react'
-
-import { connect } from 'react-redux'
-// import { 
-
-// } from '../../redux/actions'
 import Entities from './entities'
 import { checkBound } from './utils'
 import Map from './maps.js'
@@ -13,15 +8,12 @@ import Map from './maps.js'
  *
  *  1. Either an Array or a 2d Array
  *  2. Function renders this array to canvas objects
- *  3. Function that takes the initial array and 
- *     use it to calculate boundaries.
- *  4. Use boundaries and check player position.
  * 
  * @class Main
  * @extends {Component}
 **/
 
-class Main extends Component {
+export default class Main extends Component {
   constructor() {
     super()
 
@@ -43,19 +35,21 @@ class Main extends Component {
       r: 10
     }
     this.speed = 5
-    this.fps = 30
     this.locked = true
     this.entities = 0
     this.img = 0
-    this.map = 0
-    this.mapLoc = [0, 0]
-    this.mapSize = [1920, 1080]
+    this.map = {
+      loc: [0,0],
+      size: [1920,1080]
+    }
+
     this.keyMap = {
       87: 'up',
       83: 'down',
       65: 'left',
       68: 'right'
     }
+
     this.move = {
       up: false,
       down: false,
@@ -86,7 +80,7 @@ class Main extends Component {
     ]
 
     this.img = new Image()
-    this.img.src = this.entities.createMap(this.mapSize, this.box, this.box2)
+    this.img.src = this.entities.createMap(this.map.size, this.box, this.box2)
 
     this.maps.init()
 
@@ -96,7 +90,6 @@ class Main extends Component {
 
     this.context = this.canvas.getContext('2d')
     this.drawAll()
-    window.requestAnimationFrame(this.animation)
   }
 
   playerMoveStart = (e) => {
@@ -111,14 +104,8 @@ class Main extends Component {
 
   drawAll = () => {
     this.entities.background(this, this.state.width, this.state.height)
-    this.context.drawImage(this.img, this.mapLoc[0], this.mapLoc[1])
+    this.context.drawImage(this.img, this.map.loc[0], this.map.loc[1])
     this.entities.you(this, this.youEntity.x, this.youEntity.y, this.youEntity.r)
-    // this.entities.testPixel(
-    //   this,
-    //   this.youEntity.x - this.youEntity.r -2,
-    //   this.youEntity.y,
-    //   1
-    // )
   } 
 
 
@@ -131,7 +118,7 @@ class Main extends Component {
       ).data
 
       if (checkBound(data)) {
-        this.mapLoc[1] += this.speed
+        this.map.loc[1] += this.speed
       }
     }
 
@@ -143,7 +130,7 @@ class Main extends Component {
       ).data
 
       if (checkBound(data)) {
-        this.mapLoc[1] -= this.speed
+        this.map.loc[1] -= this.speed
       }
     }
 
@@ -155,7 +142,7 @@ class Main extends Component {
       ).data
 
       if (checkBound(data)) {
-        this.mapLoc[0] += this.speed
+        this.map.loc[0] += this.speed
       }
     }
 
@@ -167,15 +154,29 @@ class Main extends Component {
       ).data
 
       if (checkBound(data)) {
-        this.mapLoc[0] -= this.speed
+        this.map.loc[0] -= this.speed
       }
     }
   }
 
   animation = () => {
-    this.update()
+    if (!this.locked) {
+      this.update()
+      this.drawAll()
+      window.requestAnimationFrame(this.animation)
+    }
+  }
+
+  handleStart = (e) => {
+    e.preventDefault()
+    this.locked = false
     this.drawAll()
     window.requestAnimationFrame(this.animation)
+  }
+
+  handleStop = (e) => {
+    e.preventDefault()
+    this.locked = true
   }
 
   render() {
@@ -187,19 +188,9 @@ class Main extends Component {
           height={this.state.height}
         />
 
-        {/* <button onClick={this.handleInterval}>move box</button> */}
-        {/* <button onClick={this.handleIntervalClear}>clear</button> */}
+        <button onClick={this.handleStart}>Start</button>
+        <button onClick={this.handleStop}>Stop</button>
       </main>
     )
   }
 }
-
-const mapStateToProps = (state, props) => {
-  return {}
-}
-
-const mapDispatchToProps = (dispatch, props) => {
-  return {}
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Main)
--- src/components/main/maps.js
+++ src/components/main/maps.js
@@ -1,24 +1,148 @@
 import { rand } from './utils'
 
+const mapSize = [192, 108] // 1920 x 1080 
+let mapArr = []
+let roomObj = {
+  dimX: 0, 
+  dimY: 0,
+  locX: 0, 
+  locY: 0,
+  id: 0
+}
+
 export default class Map {
-  init() {
-    mapArr.push(initRoom())
-    console.log(mapArr)
+  constructor() {
+    [ this.min , this.max ] = [ 2, 30 ]
   }
 
-  gen() {
-    function* generator(i) {
+  init() {
+    mapArr.push(initRoom())
+    console.log(mapArr[0])
 
-      yield i
+    let topLeft = [mapArr[0].locX, mapArr[0].locY]
+    let bottomRight = [
+      mapArr[0].locX + mapArr[0].dimX, 
+      mapArr[0].locY + mapArr[0].dimY
+    ]
+
+    // Distances 
+    let dist = {
+      top: topLeft[1] - 0,
+      right: mapSize[0] - bottomRight[0],
+      bottom: mapSize[1] - bottomRight[1],
+      left: topLeft[0] - 0
     }
 
-    generator()
+    let big = ''
+    if (dist.top > dist.right) {
+      big = 'top'
+    } else {
+      big = 'right'
+    }
+
+    if (dist[big] < dist.bottom) {
+      big = 'bottom'
+    }
+
+    if (dist[big] < dist.left) {
+      big = 'left'
+    }
+
+    console.log(dist)
+    console.log(dist[big])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // this.createRoom(1)
+
+    // this.gen = this.generator()
+
+    // console.log(this.gen.next().value)
+    // console.log(this.gen.next().value)
+    // console.log(this.gen.next().value)
+    // console.log(this.gen.next().value)
+  }
+
+  // * generator() {
+  //   let [ min, max ] = [ 2, 30 ]
+  //   let i = 0
+
+  //   while(true) {
+  //     i++
+  //     yield {
+  //       dimX: rand(min, max),
+  //       dimY: rand(min, max),
+  //       locX: rand(this.locXmin, this.locXmax),
+  //       locY: rand(this.locYmin, this.locYmax),
+  //       id: i
+  //     }
+  //   }
+  // }
+
+  createRoom(x) {
+    // let finished = false
+    let room = { ...roomObj }
+
+    room = {
+      dimX: rand(this.min, this.max),
+      dimY: rand(this.min, this.max),
+      locX: rand(0, mapSize[0]),
+      locY: rand(0, mapSize[1]),
+      id: x
+    }
+
+    mapArr.push(room)
+
+    // if (wall) {
+    //   finished = true
+    // }
+
+    // if (!finished) {
+    //   this.createRoom(x++)
+    // }
+  }
+
+  spacing() {
+
   }
 }
 
+const initRoom = () => {
+  let [ min, max ] = [ 7, 25 ]
+  let [ dimX, dimY ] = [ rand(min, max), rand(min, max) ]
+  let room = { ...roomObj }
 
-const mapSize = [192, 108] // 1920 x 1080 
-let mapArr = []
+  room = {
+    dimX: dimX, 
+    dimY: dimY,
+    locX: rand(0, mapSize[0]-dimX), 
+    locY: rand(0, mapSize[1]-dimY),
+    id: 0
+  }
+
+  return room
+}
+
 // const rand = Math.ceil(Math.random() * 10)
 
 // if (rand < 10 && rand > 90) { // 20% room
@@ -48,22 +172,4 @@ let mapArr = []
     
 //   }
 // }
-
-
-function initRoom() {
-  let min = 7
-  let max = 25
-  let room = {
-    dimX: rand(min, max), 
-    dimY: rand(min, max),
-    locX: rand(0, mapSize[0]), 
-    locY: rand(0, mapSize[1]),
-    id: 0
-  }
-
-  return room
-}
-
-
-
 
